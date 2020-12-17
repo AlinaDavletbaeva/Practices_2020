64f2fead88adf5d0ff894bbb57f806ee8b4871f9
Alina <niimura.tsucumi@gmail.com>
2020-12-17 04:04:08 +0300

    Making program for lessons 25-26 //sleep MIREA 2020

--- src/ru/ivbo_11_19/all_practices/practice25_26/Main.java
+++ src/ru/ivbo_11_19/all_practices/practice25_26/Main.java
@@ -1,119 +1,74 @@
 package ru.ivbo_11_19.all_practices.practice25_26;
 
-import java.io.File;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("C://Users//Alina//Desktop//JAVAPR//src//ru//ivbo_11_19//all_practices//practice25_26//oscar_age.csv"));
+        String line = null;
+        Scanner scanner = null;
+        int index = 0;
+        int flag = 1;
+        int maxAge = 1;
+        List<Actor> actList = new ArrayList<>();
 
-    private static final char DEFAULT_SEPARATOR = ',';
-    private static final char DEFAULT_QUOTE = '"';
-
-    public static void main(String[] args) throws Exception {
-
-        String csvFile = "/Users/Alina/Desktop/JAVAPR/src/ru/ivbo_11_19/all_practices/practice25/oscar_age.csv";
-
-        Scanner scanner = new Scanner(new File(csvFile));
-        while (scanner.hasNext()) {
-            List<String> line = parseLine(scanner.nextLine());
-
-            System.out.println(line.get(0)+" [year= " + line.get(1) + " , age=" + line.get(2) + " , name=" + line.get(3) + " , movie=" + line.get(4) +"]");
-        }
-        scanner.close();
-
-    }
-
-    public static List<String> parseLine(String cvsLine) {
-        return parseLine(cvsLine, DEFAULT_SEPARATOR, DEFAULT_QUOTE);
-    }
-
-    public static List<String> parseLine(String cvsLine, char separators) {
-        return parseLine(cvsLine, separators, DEFAULT_QUOTE);
-    }
-
-    public static List<String> parseLine(String cvsLine, char separators, char customQuote) {
-
-        List<String> result = new ArrayList<>();
-
-        //if empty, return!
-        if (cvsLine == null && cvsLine.isEmpty()) {
-            return result;
-        }
-
-        if (customQuote == ' ') {
-            customQuote = DEFAULT_QUOTE;
-        }
-
-        if (separators == ' ') {
-            separators = DEFAULT_SEPARATOR;
-        }
-
-        StringBuffer curVal = new StringBuffer();
-        boolean inQuotes = false;
-        boolean startCollectChar = false;
-        boolean doubleQuotesInColumn = false;
-
-        char[] chars = cvsLine.toCharArray();
-
-        for (char ch : chars) {
-
-            if (inQuotes) {
-                startCollectChar = true;
-                if (ch == customQuote) {
-                    inQuotes = false;
-                    doubleQuotesInColumn = false;
-                } else {
-
-                    //Fixed : allow "" in custom quote enclosed
-                    if (ch == '\"') {
-                        if (!doubleQuotesInColumn) {
-                            curVal.append(ch);
-                            doubleQuotesInColumn = true;
+        while ((line = reader.readLine()) != null) {
+            if(flag == 1){
+                flag += 1;
+                continue;
+            }
+            else {
+                Actor actor = new Actor();
+                scanner = new Scanner(line);
+                scanner.useDelimiter(", ");
+                while (scanner.hasNext()) {
+                    String data = scanner.next();
+                    if (index == 0)
+                        actor.setIndex(Integer.parseInt(data));
+                    else if (index == 1) {
+                        actor.setYear(Integer.parseInt(data));
+                    } else if (index == 2) {
+                        actor.setAge(Integer.parseInt(data));
+                        if(maxAge < actor.getAge()){
+                            maxAge = actor.getAge();
                         }
+                    } else if (index == 3) {
+                        actor.setName(data);
+                    } else if (index == 4) {
+                        scanner.useDelimiter(", ");
+                        actor.setMovie(data);
                     } else {
-                        curVal.append(ch);
+                        System.out.println("Некорректные данные:" + data);
                     }
-
+                    ++index;
                 }
-            } else {
-                if (ch == customQuote) {
+                index = 0;
+                actList.add(actor);
+            }
+        }
+        reader.close();
 
-                    inQuotes = true;
-
-                    //Fixed : allow "" in empty quote enclosed
-                    if (chars[0] != '"' && customQuote == '\"') {
-                        curVal.append('"');
+        for(int i = maxAge; i != 0; i--){
+            int counterOsc = 0;
+            String actors = "";
+            for(Actor item : actList){
+                if(i == item.getAge()){
+                    counterOsc++;
+                    if (counterOsc != 1){
+                        actors += ", ";
                     }
-
-                    //double quotes in column will hit this!
-                    if (startCollectChar) {
-                        curVal.append('"');
-                    }
-
-                } else if (ch == separators) {
-
-                    result.add(curVal.toString());
-
-                    curVal = new StringBuffer();
-                    startCollectChar = false;
-
-                } else if (ch == '\r') {
-                    //ignore LF characters
-                    continue;
-                } else if (ch == '\n') {
-                    //the end, break!
-                    break;
-                } else {
-                    curVal.append(ch);
+                    actors += item.getName();
                 }
             }
-
+            if(counterOsc != 0) {
+                System.out.println(i + " - " + counterOsc + " | " + actors + ".");
+            }
         }
-
-        result.add(curVal.toString());
-
-        return result;
     }
-
 }
