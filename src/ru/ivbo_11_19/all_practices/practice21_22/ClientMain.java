95932b0af6f79d3416fb6a82056b0b344c3dbb45
Alina <niimura.tsucumi@gmail.com>
2020-12-17 03:18:43 +0300

    Making program for lessons 21-22 //sleep MIREA 2020

---
+++ src/ru/ivbo_11_19/all_practices/practice21_22/ClientMain.java
@@ -0,0 +1,8 @@
+package ru.ivbo_11_19.all_practices.practice21_22;
+
+
+public class ClientMain {
+    public static void main(String[] args) {
+        ClientWindow clientWindow = new ClientWindow();
+    }
+}
---
+++ src/ru/ivbo_11_19/all_practices/practice21_22/ServerMain.java
@@ -0,0 +1,7 @@
+package ru.ivbo_11_19.all_practices.practice21_22;
+
+public class ServerMain {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+    }
+}
